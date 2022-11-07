5c32347aae4128fe47b26b24f8b9651d28309e36
Nar Cuenca <narc.ph@gmail.com>
2022-11-07 20:15:55 +0800

    chore: added mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 function App() {
-    return <div className="App">Hello</div>;
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="App">
+            <Typography>{count}</Typography>
+            <Button onClick={() => setCount(count + 1)}>+1</Button>
+        </div>
+    );
 }
 
 export default App;
